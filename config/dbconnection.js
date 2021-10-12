9bf4905d131cc67234d2d68f8324abb57ef41739
김동철 <80299917+ehdcjf@users.noreply.github.com>
2021-10-12 19:57:54 +0900

    Update dbconnection.js

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 const config = {
   host: 'localhost',
   user: 'root',
-  password: '1234',
+  password: 'root',
   database: 'exchange',
   multipleStatements: true,
 }
